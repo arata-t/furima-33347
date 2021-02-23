9cc5578c85a3ebda6ef4eccdc6170e1a404ae9c3
arata-t <taniguro.arata@gmail.com>
2021-02-23 20:53:13 +0900

    active strage導入まで

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item)
+      .permit(:product_name, :category_id, :price, :product_status_id, :burden_id, :area_id, :days_id, :description, :user, :image)
+      .merge(user_id: current_user)
   end
 end

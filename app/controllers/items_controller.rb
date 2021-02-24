abfffd1baf291e705eb9bfdef3c315f4fa6ff9dc
arata-t <taniguro.arata@gmail.com>
2021-02-24 11:41:48 +0900

    active_hashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
@@ -8,13 +8,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end 
   end
 
   private
   def item_params
     params.require(:item)
-      .permit(:product_name, :category_id, :price, :product_status_id, :burden_id, :area_id, :days_id, :description, :user, :image)
-      .merge(user_id: current_user)
+      .permit(:product_name, :category_id, :price, :product_status_id, :burden_id, :area_id, :days_id, :description, :image)
+      .merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  validates :product_name, presence: true
+  validates :price,        presence: true
+  validates :description,  presence: true
+
+  validates :category_id,  numericality: {other_than: 1 }
 end

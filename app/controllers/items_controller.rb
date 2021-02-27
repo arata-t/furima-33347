425e4867b65502ec77a58747a76315b211d82a71
arata-t <taniguro.arata@gmail.com>
2021-02-27 20:10:17 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,6 +33,11 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    redirect_to root_path if item.destroy
+  end
+
   private
 
   def item_params

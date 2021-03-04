9e12ce652ee00f3ccd0ce394c32b98450bb7b4d1
arata-t <taniguro.arata@gmail.com>
2021-03-04 20:14:02 +0900

    order,delivery　db保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,21 @@
 class OrdersController < ApplicationController
   def index
+    @item_order = ItemOrder.new
+  end
+
+  def create
+    @item_order = ItemOrder.new(order_params)
+    binding.pry
+    if @item_order.valid?
+      @item_order.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:item_order).permit(:postal_code, :area_id, :city, :house_num, :building_name, :phone, :item_id).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
---
+++ app/models/item_order.rb
@@ -0,0 +1,16 @@
+class ItemOrder
+  include ActiveModel::Model
+  attr_accessor :postal_code, :area_id, :city, :house_num, :building_name, :phone, :item_id, :user_id
+
+  validates :postal_code,             format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :area_id,           numericality: { other_than: 1, message: '配送元の地域を選択してください' }
+  validates :city,                    format: {with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Include hyphen(-)"}
+  validates :house_num,               format: {with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Include hyphen(-)"}
+  validates :building_name,           format: {with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Include hyphen(-)"}
+  validates :phone,                   format: {with: /(0{1}\d{9,10})/     , message: "is invalid. Include hyphen(-)"}
+
+  def save
+    order = Order.create( item_id: item_id, user_id: user_id)
+    Delivery.create(postal_code: postal_code, area_id: area_id, city: city, house_num: house_num, building_name: building_name, phone: phone, order_id: order.id)
+  end
+end

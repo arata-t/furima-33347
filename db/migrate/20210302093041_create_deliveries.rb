9a4d83580074c076ab51f32061d631d155e3b3fc
arata-t <taniguro.arata@gmail.com>
2021-03-07 08:07:42 +0900

    db　建物名のnull: false　消去

--- db/migrate/20210302093041_create_deliveries.rb
+++ db/migrate/20210302093041_create_deliveries.rb
@@ -5,7 +5,7 @@ class CreateDeliveries < ActiveRecord::Migration[6.0]
       t.integer       :area_id,       null: false
       t.string        :city,          null: false
       t.string        :house_num,     null: false
-      t.string        :building_name, null: false
+      t.string        :building_name
       t.string        :phone,         null: false
       t.references    :order,         null: false,foreign_key: true
       t.timestamps
